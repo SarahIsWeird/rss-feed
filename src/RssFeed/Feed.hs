c4dc78dc9aaf3e22b6b187d65fe83e726b2cf3d1
SarahIsWeird <morningcrafter0603@gmail.com>
2023-03-30 16:06:09 +0200

    Auto-create config file

--- src/RssFeed/Feed.hs
+++ src/RssFeed/Feed.hs
@@ -1,13 +1,21 @@
 module RssFeed.Feed where
 
 import Control.Exception (Exception (displayException), SomeException (SomeException), catch)
+import Control.Monad
 import Data.Aeson (decode)
 import qualified Data.ByteString.Lazy as BL
 import RssFeed.Types (Feeds)
+import System.Directory
 
 readFeedConfig :: FilePath -> IO (Either String Feeds)
 readFeedConfig path = do
-  contents <- catch (fmap Right (BL.readFile path)) (\(SomeException e) -> pure . Left . displayException $ e)
+  configExists <- doesFileExist "feeds.json"
+  -- Look. I'm not in the mood to deal with ByteStrings right now, and I don't get them really either. :D
+  unless configExists $ writeFile "feeds.json" "{}"
+  contents <-
+    catch
+      (fmap Right (BL.readFile path))
+      (\(SomeException e) -> pure . Left . displayException $ e)
   case contents of
     Left e -> pure (Left e)
     Right c ->
